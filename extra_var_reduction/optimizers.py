fixed EGVR with Lookahead

extra_var_reduction/optimizers.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import torch.functional as F
 from torch.optim import Optimizer
+from lookahead import Lookahead
 
 
 class Extragrad_Var_Reduction():
@@ -12,13 +13,16 @@ class Extragrad_Var_Reduction():
         self, parameters, dual_parameters,  *,
         lr=1e-3, betas=(0.9, 0.999),
         alpha=0.9, p=0.95,
-        optimizer=Optimizer
+        optimizer=Optimizer,
+        use_lookahead=False
     ):
         self.lr = lr
         self.alpha = alpha
         self.p = p
         defaults = dict(lr=lr, betas=betas)
         self.optimizer = optimizer(parameters, **defaults)
+        if use_lookahead:
+            self.optimizer = Lookahead(self.optimizer, k=5, alpha=0.5)
         self.dual_optimizer = optimizer(dual_parameters, **defaults)
     
     @torch.no_grad()

extra_var_reduction/train_min_max.py
@@ -1,10 +1,8 @@
-import shutil, fire, os, copy
+import shutil, os, copy #fire
 
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-
-import wandb
 
 import optimizers
 from metrics import get_discriminator_loss, get_generator_loss
@@ -13,12 +11,12 @@ from utils import elapsed, load_mnist, sampler
 from models import GeneratorCNN28, DiscriminatorCNN28, save_models
 
 
-def train(epochs=5, batch_size=1024, 
+def train(epochs=5, batch_size=1024, use_lookahead=False,
           mini_batch_size=64, p=None, lr=1e-4,
           betas=(0.9, 0.999), alpha=None, eval_every=100, 
           n_workers=4, device=torch.device('cuda'),
           grad_max_norm=1, out_dir=None, shuffle=True, 
-          pretrained_clf_path="./mnist.pth", seed=None
+          pretrained_clf_path='/content/drive/MyDrive/MLO_proj/Data/models/mnist.pth', seed=None
 ):
     if p is None:
         p = 2*mini_batch_size/batch_size
@@ -78,7 +76,8 @@ def train(epochs=5, batch_size=1024,
         lr=lr,
         alpha=alpha,
         p=p,
-        optimizer=optimizer
+        optimizer=optimizer,
+        use_lookahead=use_lookahead
     )
     
     #LBLs
@@ -177,5 +176,5 @@ def train(epochs=5, batch_size=1024,
                               D=D, G=G, iteration=i)
                     
                 
-if __name__=="__main__":
-    fire.Fire(train)
+#if __name__=="__main__":
+#    fire.Fire(train)

scripts/models.py
@@ -23,7 +23,6 @@ class DiscriminatorCNN28(nn.Module):
                              "number of output channels, the dimension of the noise "
                              "vector, as well as the depth of the convolution kernels.")
         super(DiscriminatorCNN28, self).__init__()
-        # _conv = nn.utils.spectral_norm(nn.Conv2d) if spectral_norm else nn.Conv2d
         _apply_sn = lambda x: nn.utils.spectral_norm(x) if spectral_norm else x
         self.img_channels = img_channels
         self.img_size = img_size
@@ -129,6 +128,8 @@ class MLP_mnist(nn.Module):
     return x, fc2_out
 
 
+#creating architecture for pretrained MNIST classifier, which we use for 
+#IS and FID metric evaluation
 def pretrained_mnist_model(input_dims=784, n_hiddens=[256, 256], n_class=10, 
                            pretrained=None):
     model = MLP_mnist(input_dims, n_hiddens, n_class)

scripts/train.py
@@ -102,7 +102,7 @@ def train(dataset, iterations, batch_size=32, lr=1e-4,
   working_time = 0
   for i in range(iterations):
 
-    # STEP 1: get G_{t+1} (G_extra)
+    # STEP 1: update G_extra
     elapsed()
     if extragrad:
       optimizerG_extra.zero_grad()
@@ -111,7 +111,7 @@ def train(dataset, iterations, batch_size=32, lr=1e-4,
       lossG.backward()
       optimizerG_extra.step()
 
-    # STEP 3: Get D_{t+1} (D_extra)
+    # STEP 2: update D_extra
     if extragrad:
       optimizerD_extra.zero_grad()
       x_real, _ = sampler()
@@ -123,7 +123,7 @@ def train(dataset, iterations, batch_size=32, lr=1e-4,
       lossD.backward()
       optimizerD_extra.step()
 
-    # STEP 2: D optimization step using G_extra
+    # STEP 3: D optimization step using G_extra
     x_real, _ = sampler()
     x_real = x_real.to(device)
     z = torch.randn(batch_size, G.noise_dim, device=device)
@@ -153,13 +153,6 @@ def train(dataset, iterations, batch_size=32, lr=1e-4,
       update_avg_gen(G, G_avg, i)
       update_ema_gen(G, G_ema, beta_ema=0.9999)
 
-    #if lookahead and (i+1) % lookahead_k == 0: 
-    #  optimizerG.update_lookahead()
-    #  optimizerD.update_lookahead()
-
-    #if i % 20000 == 0:
-    #  save_models(G, D, optimizerG, optimizerD, out_dir, suffix=f"{i}")
-
     working_time += elapsed()
     # Just plotting things
     if i % eval_every == 0 or i == iterations-1:
@@ -173,7 +166,8 @@ def train(dataset, iterations, batch_size=32, lr=1e-4,
       print(f"Iter {i}: Mean proba from D(G(z)): {mean_proba:.4f} +/- {std_proba:.4f}")
       plot_func(samples.detach().cpu(), time_tick=working_time, D_loss=lossD.item(), G_loss=lossG.item(), D=D, G=G, iteration=i, G_avg=G_avg, G_ema=G_ema)
 
-def save_models(G, D, out_dir, suffix, opt_G=None, opt_D=None):
+
+def save_models(G, D, opt_G, opt_D, out_dir, suffix):
   torch.save(G.state_dict(), os.path.join(out_dir, f"gen_{suffix}.pth"))
   torch.save(D.state_dict(), os.path.join(out_dir, f"disc_{suffix}.pth"))
   if opt_G is not None: